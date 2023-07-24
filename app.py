cd4448c0de0f09cbeffe6076720324fb7c165ee6
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-24 19:21:46 +0200

    Add foreign Key

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, select, col
+from sqlmodel import Field, SQLModel, create_engine, Relationship
+
+
+class Team(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
 
 
 class Hero(SQLModel, table=True):
@@ -6,6 +12,7 @@ class Hero(SQLModel, table=True):
     name: str = Field(index=True)
     secret_name: str
     age: int | None = Field(default=None, index=True)
+    team_id: int | None = Field(default=None, foreign_key="team.id")
 
 
 sqlite_file_name = "database.db"
@@ -19,70 +26,9 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-def create_heroes():
-    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
-    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
-    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
-    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
-    hero_7 = Hero(name="Captain North America",
-                  secret_name="Esteban Rogelios", age=93)
-
-    with Session(engine) as session:
-        session.add(hero_1)
-        session.add(hero_2)
-        session.add(hero_3)
-        session.add(hero_4)
-        session.add(hero_5)
-        session.add(hero_6)
-        session.add(hero_7)
-
-        session.commit()
-
-
-def select_heroes():
-    with Session(engine) as session:
-        statement = select(Hero).where(Hero.age > 25).limit(3)
-        results = session.exec(statement)
-        heroes = results.all()
-        print(heroes)
-
-
-def update_heroes():
-    with Session(engine) as session:
-        statement = select(Hero).where(Hero.name == "Spider-Boy")
-        results = session.exec(statement)
-        hero = results.one()
-        print("Hero:", hero)
-
-        hero.age = 16
-        session.add(hero)
-        session.commit()
-        session.refresh(hero)
-        print("Updated hero:", hero)
-
-
-def delete_heroes():
-    with Session(engine) as session:
-        statement = select(Hero).where(Hero.name == "Spider-Boy")
-        results = session.exec(statement)
-        hero = results.one()
-        print("Hero", hero)
-
-        session.delete(hero)
-        session.commit()
-
-        print(hero)
-
-
 def main():
     create_db_and_tables()
-    create_heroes()
-    select_heroes()
-    update_heroes()
-    delete_heroes()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
