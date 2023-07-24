d4c0e376e71a54697f4cdadeda86836f63fbd60b
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-24 15:15:24 +0200

    Create Table

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ class Hero(SQLModel, table=True):
     name: str
     secret_name: str
     age: int | None = None
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+SQLModel.metadata.create_all(engine)
