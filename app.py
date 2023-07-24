19c771e0954833348d93870b4d427a529b9af56a
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-24 17:51:49 +0200

    Get one row(first that satisfies where statement)

--- app.py
+++ app.py
@@ -44,7 +44,9 @@ def create_heroes():
 def select_heroes():
     with Session(engine) as session:
         statement = select(Hero).where(col(Hero.age) > 35)
-        results = session.exec(statement).all()
+        results = session.exec(statement)
+        hero = results.first()
+        print("Hero:", hero)
 
 
 def main():
