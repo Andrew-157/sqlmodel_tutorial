168d29871d1de0740ec38482abbc1672d8206140
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-25 08:40:51 +0200

    Create Instances with Relationship Attributes

--- app.py
+++ app.py
@@ -30,8 +30,37 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
+def create_heroes():
+    with Session(engine) as session:
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(
+            name="Z-Force", headquarters="Sister Margaret’s Bar")
+
+        hero_deadpond = Hero(
+            name="Deadpond", secret_name="Dive Wilson", team=team_z_force
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man", secret_name="Tommy Sharp", age=48, team=team_preventers
+        )
+        hero_spider_boy = Hero(
+            name="Spider-Boy", secret_name="Pedro Parqueador")
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Created hero:", hero_deadpond)
+        print("Created hero:", hero_rusty_man)
+        print("Created hero:", hero_spider_boy)
+
+
 def main():
     create_db_and_tables()
+    create_heroes()
 
 
 if __name__ == "__main__":
