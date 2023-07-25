7d33783e5485f8c3def667135ecd7fb13ff55fdd
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-25 08:36:23 +0200

    Relationship attributes

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ class Team(SQLModel, table=True):
     name: str = Field(index=True)
     headquarters: str
 
+    heroes: list["Hero"] = Relationship(back_populates="team")
+
 
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -13,6 +15,8 @@ class Hero(SQLModel, table=True):
     secret_name: str
     age: int | None = Field(default=None, index=True)
     team_id: int | None = Field(default=None, foreign_key="team.id")
+
+    team: "Team" | None = Relationship(back_populates="heroes")
 
 
 sqlite_file_name = "database.db"
@@ -26,52 +30,8 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-def create_heroes():
-    with Session(engine) as session:
-        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
-        team_z_force = Team(
-            name="Z-Force", headquarters="Sister Margaret’s Bar")
-        session.add(team_preventers)
-        session.add(team_z_force)
-        session.commit()
-
-        hero_deadpond = Hero(
-            name="Deadpond", secret_name="Dive Wilson", team_id=team_z_force.id
-        )
-        hero_rusty_man = Hero(
-            name="Rusty-Man",
-            secret_name="Tommy Sharp",
-            age=48,
-            team_id=team_preventers.id,
-        )
-        hero_spider_boy = Hero(
-            name="Spider-Boy", secret_name="Pedro Parqueador")
-        session.add(hero_deadpond)
-        session.add(hero_rusty_man)
-        session.add(hero_spider_boy)
-        session.commit()
-
-        session.refresh(hero_deadpond)
-        session.refresh(hero_rusty_man)
-        session.refresh(hero_spider_boy)
-
-        print("Created hero:", hero_deadpond)
-        print("Created hero:", hero_rusty_man)
-        print("Created hero:", hero_spider_boy)
-
-
-def select_heroes():
-    with Session(engine) as session:
-        statement = select(Hero, Team).join(Team, isouter=True)
-        results = session.exec(statement)
-        for hero, team in results:
-            print("Hero:", hero, "Team:", team)
-
-
 def main():
     create_db_and_tables()
-    create_heroes()
-    select_heroes()
 
 
 if __name__ == "__main__":
